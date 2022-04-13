aca37e41b23cb74578097a12eddbc740dc28a19d
TomKan <tomkan0909@gmail.com>
2022-04-13 17:15:20 -0400

    params for drawing boxes

--- boundingboxannotate.js
+++ boundingboxannotate.js
@@ -1,31 +1,63 @@
 (function (window, document) {
     
-    function BoundingBoxAnnotate (props){
+    // Left and right coordinate 
+    function BoundingBox (leftBottom, rightTop) {
+        this.xLeft = leftBottom[0];
+        this.yLeft = leftBottom[1];
+        this.xRight = rightTop[0];
+        this.yRight = rightTop[1];
+    }
+    
+    BoundingBox.prototype = {
+        getWidth: function() {
+            return (this.xRight - this.xLeft) * 100;
+        },
+        getHeight: function() {
+            return (this.yRight - this.yLeft) * 100;
+        }
+    }
+
+    function processBoundingBoxList(boundingBoxList){
+        return boundingBoxList.map(boundingBox => new BoundingBox(boundingBox[0], boundingBox[1]))
+    }
+
+    function BoundingBoxAnnotate (props) {
         this.id = props.id;
         this.image = props.image;
+        this.boundingBoxes = processBoundingBoxList(props.boundingBoxes)
     }
 
     BoundingBoxAnnotate.prototype = {
+        
         createBoundingBoxAnnotate: function() {
             const div = document.getElementById(this.id);
             let image = document.createElement('img');
             image.src = this.image;
             div.appendChild(image);
-        }
+            this._drawBoundingBox(div);
+        },
+        _drawBoundingBox: function(parentDiv) {
+            parentDiv.setAttribute("style", "position:relative;display:inline-block")
+            this.boundingBoxes.forEach((boundingBox, index) => {
+                const boundingBoxDiv = document.createElement('div');
+                boundingBoxDiv.setAttribute("style", `position:absolute;bottom:${boundingBox.yLeft * 100}%;
+                left:${boundingBox.xLeft * 100}%;width:${boundingBox.getWidth()}%;height:${boundingBox.getHeight()}%;border:solid 3px black;border-radius:2%;z-index:${index + 1}`);
+                parentDiv.appendChild(boundingBoxDiv);
+            }); 
+        },
+
+        // _generateModal: function() {
+
+        // }
+
+        // _generateTable: function() {
+
+        // }
+
+        
     }
+
+    // allows function BoundingBoxAnnotate to be accessible globally on the window object(represents browser)
+    window.BoundingBoxAnnotate = window.BoundingBoxAnnotate || BoundingBoxAnnotate;
 
 })(window, window.document);
-
-function BoundingBoxAnnotate (props){
-    this.id = props.id;
-    this.image = props.image;
-}
-
-BoundingBoxAnnotate.prototype = {
-    createBoundingBoxAnnotate: function() {
-        const div = document.getElementById(this.id);
-        let image = document.createElement('img');
-        image.src = this.image;
-        div.appendChild(image);
-    }
-}
--- example.js
+++ example.js
@@ -1,10 +1,9 @@
 "use strict"
-console.log('Entered example.js')
 
 const props = {
     id: 'test',
-    image: 'image0.jpeg'
+    image: 'image0.jpeg',
+    boundingBoxes: [[[0.1, 0.1], [0.2, 0.2]], [[0.4, 0.4], [0.7, 0.8]]]
 }
 const generator = new BoundingBoxAnnotate(props);
 generator.createBoundingBoxAnnotate();
-
