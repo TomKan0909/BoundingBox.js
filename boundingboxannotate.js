2070069dd98806f16d6f40b043e011da4242b422
TomKan <tomkan0909@gmail.com>
2022-04-14 01:12:34 -0400

    make table of content draggable

--- boundingboxannotate.js
+++ boundingboxannotate.js
@@ -28,6 +28,59 @@
         this.labels = props.labels;
     }
 
+    // https://www.w3schools.com/howto/howto_js_draggable.asp
+    function _dragElement(element) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (element.firstChild) {
+          // if present, the header is where you move the DIV from:
+          element.firstChild.onmousedown = dragMouseDown;
+        } else {
+          // otherwise, move the DIV from anywhere inside the DIV:
+          element.onmousedown = dragMouseDown;
+        }
+      
+        function dragMouseDown(e) {
+          e = e || window.event;
+          e.preventDefault();
+          // get the mouse cursor position at startup:
+          pos3 = e.clientX;
+          pos4 = e.clientY;
+          document.onmouseup = closeDragElement;
+          // call a function whenever the cursor moves:
+          document.onmousemove = elementDrag;
+        }
+      
+        function elementDrag(e) {
+          e = e || window.event;
+          e.preventDefault();
+          // calculate the new cursor position:
+          pos1 = pos3 - e.clientX;
+          pos2 = pos4 - e.clientY;
+          pos3 = e.clientX;
+          pos4 = e.clientY;
+          // set the element's new position:
+          console.log('top: ', element.parentElement.offsetTop);
+          
+          //https://stackoverflow.com/questions/9872128/get-bottom-and-right-position-of-an-element
+          // Returns bottom offset value + or - from viewport top
+          function offsetBottom(el, i) { i = i || 0; return $(el)[i].getBoundingClientRect().bottom }
+
+          // Returns right offset value
+          function offsetRight(el, i) { i = i || 0; return $(el)[i].getBoundingClientRect().right }
+          console.log('bottom: ', offsetBottom(element.parentElement));
+          const parentElement = element.parentElement;
+          
+          element.style.top = Math.min((Math.max(0, (element.offsetTop - pos2))), offsetBottom(parentElement) - element.offsetHeight - parentElement.offsetTop)  + "px";
+          element.style.left = Math.min((Math.max(0, element.offsetLeft - pos1)), offsetRight(parentElement) - element.offsetWidth - parentElement.offsetLeft) + "px";
+        }
+      
+        function closeDragElement() {
+          // stop moving when mouse button is released:
+          document.onmouseup = null;
+          document.onmousemove = null;
+        }
+      }
+
     BoundingBoxAnnotate.prototype = {
         
         createBoundingBoxAnnotate: function() {
@@ -47,15 +100,24 @@
                 // Create BoundingBox
                 const boundingBoxDiv = document.createElement('div');
                 boundingBoxDiv.setAttribute("style", `position:absolute;bottom:${boundingBox.yLeft * 100}%;
-                left:${boundingBox.xLeft * 100}%;width:${boundingBox.getWidth()}%;height:${boundingBox.getHeight()}%;border:solid 3px black;border-radius:2%;z-index:${index + 1}`);
-                
-                // Add Label
-                const paragraphLabel = document.createElement('p');
-                const label = document.createTextNode(this.labels[index]);
-                paragraphLabel.appendChild(label);
-                boundingBoxDiv.appendChild(paragraphLabel);
+                left:${boundingBox.xLeft * 100}%;width:${boundingBox.getWidth()}%;height:${boundingBox.getHeight()}%;border:solid 3px black;border-radius:2%;border-top-left-radius:0;cursor:pointer;z-index:${index + 1}`);
+                boundingBoxDiv.addEventListener('click', function(e) {
+                    console.log('Clicked : ', e);
+                });
 
+                // Add Label
+                const labelDiv = document.createElement('div');
+                labelDiv.setAttribute("style", `position:absolute;bottom:${boundingBox.yRight * 100 + 0.5}%;
+                left:${boundingBox.xLeft * 100}%;background-color:black;color:white;border-radius:2%;padding:1px 5px;z-index:${index}`);
+                const spanLabel = document.createElement('span');
+                const label = document.createTextNode(this.labels[index]);
+                
+                spanLabel.appendChild(label);
+                labelDiv.appendChild(spanLabel);
+
+                // Append divs to parent div
                 parentDiv.appendChild(boundingBoxDiv);
+                parentDiv.appendChild(labelDiv)
             }); 
         },
 
@@ -65,7 +127,7 @@
 
         _generateLegend: function(parentDiv) {
             const legendDiv = document.createElement('div');
-            legendDiv.setAttribute("style", `position:absolute;bottom:0%;right:0%;height:30%;width:20%;border: solid 3px grey;border-radius:2%;background-color:white`);
+            legendDiv.setAttribute("style", `position:absolute;bottom:0%;right:0%;height:30%;width:20%;border: solid 3px grey;border-radius:2%;background-color:white;z-index:999999`);
             
             const list = document.createElement('ul');
             list.setAttribute("style", "list-style-type:none")
@@ -86,11 +148,18 @@
 
                 list.appendChild(listElement);
             })
+            const legendHeaderDiv = document.createElement('div');
+            legendHeaderDiv.setAttribute('id', 'legendHeader');
+            legendHeaderDiv.setAttribute('style', 'cursor:move');
+            const legendHeaderText = document.createTextNode('Labels');
+            legendHeaderDiv.appendChild(legendHeaderText);
+            legendDiv.appendChild(legendHeaderDiv);
+
             legendDiv.appendChild(list);
             parentDiv.appendChild(legendDiv);
+            _dragElement(legendDiv);
         }
 
-        
     }
 
     // allows function BoundingBoxAnnotate to be accessible globally on the window object(represents browser)
